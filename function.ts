c548d3fe4cbe49600c7e6af8e9aa49196d5ae255
mohammadaamir1102 <86870356+mohammadaamir1102@users.noreply.github.com>
2023-08-01 17:32:26 +0530

    function in ts

--- function.ts
+++ function.ts
@@ -1,15 +1,42 @@
 let sumOfTwoNum: number
-function addTwo(num1: number, num2: number): number{
-  return num1 +  num2;
+function addTwo(num1: number, num2: number): number {
+  return num1 + num2;
 }
-sumOfTwoNum = addTwo(10,10);
+sumOfTwoNum = addTwo(10, 10);
 console.log("sum of two number is " + sumOfTwoNum)
 
 // ---------------------------------------------------------------
 
 let upperCaseName: string
-function getUpperCase(value: string): string{
+function getUpperCase(value: string): string {
   return value.toUpperCase();
 }
 upperCaseName = getUpperCase("Aamir khan");
 console.log("name converted int upper case " + upperCaseName)
+
+// ---------------------------------------------------------------
+
+const getHello = (val: string): string => {
+  return `hello ${val}`
+}
+console.log(getHello("Aamir"))
+
+// ---------------------------------------------------------------
+
+const myName: string[] = ["Aamir", "Aasim", "Aaquib"]
+myName.map((item): string => {
+  return `names is ${item}`
+})
+
+// ---------------------------------------------------------------
+
+function consolePrint(value: string): void {
+  console.log(value);
+
+}
+
+// ---------------------------------------------------------------
+
+function handleError(error: string): never {
+  throw new Error(error)
+}
